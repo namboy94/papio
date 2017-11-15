41756f79f828caf9abed220e53da6d2007dbbd71
Hermann Krumrey <hermann@krumreyh.com>
2017-11-15 09:48:43 +0100

    Add license headers

--- legacy/finance-program-professional/program/objects/Wallet.py
+++ legacy/finance-program-professional/program/objects/Wallet.py
@@ -1,4 +1,23 @@
 """
+Copyright 2015-2017 Hermann Krumrey <hermann@krumreyh.com>
+
+This file is part of finance-program-professional.
+
+finance-program-professional is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+finance-program-professional is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with finance-program-professional.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
 Wallet
 class that models a wallet as an object
 
